9bc0a0d8ad8235e018e27196da38f703b227d92a
Nick Jennings <nick@silverbucket.net>
2026-05-14 16:00:53 +0200

    Throttle frame consumption to video native fps

--- vglr.py
+++ vglr.py
@@ -9,10 +9,11 @@ import av
 
 VIDEO_PATH = 'videos/fb_rev_wet_mop_reimagined.mp4'
 
-# Read video dimensions before GL init
+# Read video metadata before GL init
 with av.open(VIDEO_PATH) as _c:
     _s = _c.streams.video[0]
     VIDEO_W, VIDEO_H = _s.width, _s.height
+    VIDEO_FPS = float(_s.average_rate or _s.guessed_rate or 30)
 
 frame_queue: queue.Queue = queue.Queue(maxsize=4)
 
@@ -66,15 +67,19 @@ class VGLRApp(mglw.WindowConfig):
         self.texture = self.ctx.texture((VIDEO_W, VIDEO_H), 3)
         self.prog['video'] = 0
         self.texture.use(location=0)
+        self.frame_interval = 1.0 / VIDEO_FPS
+        self.last_frame_time = 0.0
         threading.Thread(target=decode_loop, args=(VIDEO_PATH,), daemon=True).start()
 
     def on_render(self, time, frametime):
         self.ctx.clear()
-        try:
-            frame = frame_queue.get_nowait()
-            self.texture.write(frame.tobytes())
-        except queue.Empty:
-            pass
+        if time - self.last_frame_time >= self.frame_interval:
+            try:
+                frame = frame_queue.get_nowait()
+                self.texture.write(frame.tobytes())
+                self.last_frame_time = time
+            except queue.Empty:
+                pass
         self.vao.render(moderngl.TRIANGLE_STRIP)
 
     def key_event(self, key, action, modifiers):
